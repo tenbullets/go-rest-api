805d7d88a57e8afdb515c5d72ece017875d2cd21
tenbullets <lnayc368@gmail.com>
2024-09-24 17:35:05 +0300

    add db

--- http-rest-api/go.mod
+++ http-rest-api/go.mod
@@ -7,6 +7,7 @@ require github.com/BurntSushi/toml v1.4.0
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
---
+++ http-rest-api/internal/app/store/userrepository_test.go
@@ -0,0 +1,41 @@
+package store_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tenbullets/go-rest-api.git/internal/app/model"
+	"github.com/tenbullets/go-rest-api.git/internal/app/store"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	u, err := s.User().Create(&model.User{
+		Email:             "user@example.org",
+		EncryptedPassword: "12345",
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	email := "user@example.org"
+
+	_, err := s.User().FindByEmail(email)
+	assert.Error(t, err)
+
+	s.User().Create(&model.User{
+		Email:             "user@example.org",
+		EncryptedPassword: "12345",
+	})
+
+	u, err := s.User().FindByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
